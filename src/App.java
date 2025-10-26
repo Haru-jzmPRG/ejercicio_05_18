e135875ab10b473784c6641e08c41f39ecde95b7
Jazmin <programajazminc20@gmail.com>
2025-10-26 18:27:09 +0100

    update

--- src/App.java
+++ src/App.java
@@ -1,7 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int n1;
-        int n2;
+        try {
+        int n1 = 0;
+        int n2 = 0;
 
         do {
             n1 = Integer.parseInt(System.console().readLine("Introduzca un número entero: "));
@@ -17,10 +18,15 @@ public class App {
             n2 = aux;
         }
 
-        for (int i = n1; i >= n2; i += 7) {
+        for (int i = n1; i <= n2; i += 7) {
             System.out.print(i + " ");
         }
 
         System.out.println();
+        } catch (NumberFormatException e) {
+            System.out.println("Error: Entrada no válida. Por favor, introduzca números enteros.");
+        } catch (NullPointerException e) {
+            System.out.println("Error: Entrada nula. Por favor, asegúrese de introducir un valor.");
+        }
     }
 }
